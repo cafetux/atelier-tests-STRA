779d0883fa1b152be2ff56916b7b99de914bce78
fabien maury <fabien.maury@arolla.fr>
2015-05-13 11:04:28 +0200

    remove comments

--- src/main/java/com/strator/communaute/catalogue/model/CategorieProduit.java
+++ src/main/java/com/strator/communaute/catalogue/model/CategorieProduit.java
@@ -4,18 +4,12 @@ package com.strator.communaute.catalogue.model;
  * Les types de produits
  */
 public enum CategorieProduit {
-    // 5%
+
     PRESERVATIF,
-    // 10%
     MEDICAMENT_NON_REMBOURSABLE,
-    // 2,1%
     MEDICAMENT_REMBOURSABLE,
-    // 5%
     LIVRE,
-    // 5%
     EQUIPEMENT_PERSONNE_DEPENDANTE,
-    // 20%
     HYGIENE_DENTAIRE,
-    // 20%
     CONFISERIE;
 }
